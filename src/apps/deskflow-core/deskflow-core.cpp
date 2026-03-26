f83563872ec068716af3f4c89d5289452d207c03
Nick Bolton <nick@symless.com>
2026-03-26 15:00:26 +0000

    fix: Destroy temporary QCoreApplication before creating the real one

--- src/apps/deskflow-core/deskflow-core.cpp
+++ src/apps/deskflow-core/deskflow-core.cpp
@@ -45,9 +45,10 @@ App *createApp(CoreArgParser &parser, EventQueue &events, const QString &process
 int main(int argc, char **argv)
 {
 #if defined(Q_OS_WIN)
-  // HACK to make sure settings gets the correct qApp path
-  QCoreApplication m(argc, argv);
-  m.deleteLater();
+  {
+    // HACK to make sure settings gets the correct qApp path
+    QCoreApplication m(argc, argv);
+  }
 
   ArchMiscWindows::setInstanceWin32(GetModuleHandle(nullptr));
 #endif
